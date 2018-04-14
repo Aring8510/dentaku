af13695d374f46a88e3d340b3ff8d75ca5553190
Aring8510 <Arubg5210@gmail.com>
2018-04-15 03:53:58 +0900

    コメントの追加並びにreadmeの追加

--- calculator.py
+++ calculator.py
@@ -1,24 +1,24 @@
-def calc(arg_list):
-    while len(arg_list)>1:
-        for i in range(len(arg_list)):
-            if isinstance(arg_list[i],str):
-                if arg_list[i]=='+':
-                    arg_list[i-2]+=arg_list[i-1]
+def calc(arg_list):#逆ポーランド記法を受け取り，計算結果を返すメソッド
+    while len(arg_list)>1:#リストがただ一つの数値となった場合計算を終了する
+        for i in range(len(arg_list)):#リストの個数分
+            if isinstance(arg_list[i],str):#文字列すなわち演算子ならば
+                if arg_list[i]=='+':#足し算ならば
+                    arg_list[i-2]+=arg_list[i-1]#足す
                 elif arg_list[i]=='*':
                     arg_list[i-2]*=arg_list[i-1]
                 elif arg_list[i]=='-':
                     arg_list[i-2]-=arg_list[i-1]
                 elif arg_list[i]=='/':
-                    if arg_list[i-1]!=0:
-                        arg_list[i-2]=arg_list[i-2]/arg_list[i-1]
-                    else:
-                        print("注意!!\n0で除算されました")
-                        arg_list[i-2]=0
-                del arg_list[i-1]
+                    if arg_list[i-1]!=0:#0除算出ない場合
+                        arg_list[i-2]=arg_list[i-2]/arg_list[i-1]#割る
+                    else:#0除算の場合
+                        print("注意!!\n0で除算されました")#警告を出す
+                        arg_list[i-2]=0#0を返す（仕様）
+                del arg_list[i-1]#空いたリスト2つを削除
                 del arg_list[i-1]
                 break
-    print(arg_list[i-2])
+    print(arg_list[i-2])#計算結果を出力
 
-pass_list = [4,1,2,"+",0,"/","*"]
-calc(pass_list)
+pass_list = [4,1,2,"+",0,"/","*"]#逆ポーランド記法のリストを作成
+calc(pass_list)#逆ポーランド記法を渡す
 
