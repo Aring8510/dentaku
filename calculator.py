df67f7c8f379e32d5b25151c15a4210bb82070eb
Aring8510 <Arubg5210@gmail.com>
2018-04-15 03:45:58 +0900

    0除算の場合警告を出し，除算の結果として0を返すように修正

--- calculator.py
+++ calculator.py
@@ -2,20 +2,23 @@ def calc(arg_list):
     while len(arg_list)>1:
         for i in range(len(arg_list)):
             if isinstance(arg_list[i],str):
-                if(arg_list[i]=='+'):
+                if arg_list[i]=='+':
                     arg_list[i-2]+=arg_list[i-1]
-                elif(arg_list[i]=='*'):
+                elif arg_list[i]=='*':
                     arg_list[i-2]*=arg_list[i-1]
-                elif(arg_list[i]=='-'):
+                elif arg_list[i]=='-':
                     arg_list[i-2]-=arg_list[i-1]
-                elif(arg_list[i]=='/'):
-                    arg_list[i-2]=arg_list[i-2]/arg_list[i-1]
+                elif arg_list[i]=='/':
+                    if arg_list[i-1]!=0:
+                        arg_list[i-2]=arg_list[i-2]/arg_list[i-1]
+                    else:
+                        print("注意!!\n0で除算されました")
+                        arg_list[i-2]=0
                 del arg_list[i-1]
                 del arg_list[i-1]
                 break
     print(arg_list[i-2])
 
-
-pass_list = [4,1,2,"+",2,"/","*"]
+pass_list = [4,1,2,"+",0,"/","*"]
 calc(pass_list)
 
